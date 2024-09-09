bc469ea994e4d9368cb83d5999cc5de6d0f20338
SolihovAbdulloh <abdullasolikh@mail.com>
2024-09-09 15:54:06 +0500

    2tamasala

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-//  1-masala
-
+//  1.1-masala
 
 // let n = 10;
 
@@ -7,23 +6,15 @@
 //     console.log(i)
 // }
 
-
-
-
-// 2-masala
-
+// 1.2-masala
 
 // let n = 0
- 
+
 // for(let i = 10; i > n ; i--){
 //     console.log(i)
 // }
 
-
-
-
-// 3-masala
-
+// 1.3-masala
 
 // let n = 20
 
@@ -33,10 +24,9 @@
 //     }
 // }
 
+// 1.4-masala
 
-
-
-// 4-masala
+// let n = 20
 
 // for(let i = 1 ; i < n ; i++){
 //     if(i%2==1){
@@ -44,3 +34,105 @@
 //     }
 // }
 
+// 1.5-masala
+
+// let n = +prompt("Raqam kiriting");
+
+// let faktorial = 1;
+
+// for(let i = 1 ; i <= n ; i++){
+//     faktorial *= i
+// }
+
+// console.log(faktorial)
+
+// 5 - masala
+
+// let n = prompt("so'z kiriting");
+
+// var count = 0;
+
+// for (let i = 0; i < n.length; i++) {
+//   if (
+//     n[i] == "a" ||
+//     n[i] == "i" ||
+//     n[i] == "e" ||
+//     n[i] == "u" ||
+//     n[i] == "o" ||
+//     n[i] == "A" ||
+//     n[i] == "I" ||
+//     n[i] == "E" ||
+//     n[i] == "U" ||
+//     n[i] == "O"
+//   ) {
+//     console.log(n[i]);
+//     count++;
+//   }
+// }
+// console.log(count + ' ta ' + 'unli bor' )
+
+// 6-masala
+
+// let n = prompt("So'z kiriting");
+
+// for(let i = 0 ; i < n.length ; i++){
+
+//     if (n) {
+
+//         let upper = n[0].toUpperCase();
+
+//         let result = upper + n.slice(1);
+
+//         console.log(result);
+//     } else {
+//         console.log("Hech qanday so'z kiritilmadi.");
+//     }
+// }
+
+// 7 - masala;
+
+// let n = prompt("so'z kiriting");
+
+// var count = 0;
+
+// for (let i = 0; i < n.length; i++) {
+//   if (n[i]==" ") {
+//     count++
+//   }
+// }
+// console.log(`${count} ta bo'sh joy bor`);
+
+
+
+
+// 9 - masala;
+
+// let n = prompt("so'z kiriting");
+
+// var count = 0;
+
+// for (let i = 0; i < n.length; i++) {
+//   if (
+//     n[i] == "1" ||
+//     n[i] == "2" ||
+//     n[i] == "3" ||
+//     n[i] == "4" ||
+//     n[i] == "5" ||
+//     n[i] == "6" ||
+//     n[i] == "7" ||
+//     n[i] == "8" ||
+//     n[i] == "9"
+//   ) {
+//     console.log(n[i])
+//     count++
+//   }
+// }
+// console.log(count + " ta " + "raqam bor");
+
+// 16 - masala
+
+let str = prompt('So\'z kiriting')
+let n = 10
+for(let i = 0 ; i < str.length-1 ; i--){
+        console.log(str[i])
+    }
