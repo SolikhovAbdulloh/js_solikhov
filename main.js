ee503d2b83e7e7362a427c9c1c3c7a47ac2c1ffb
SolihovAbdulloh <abdullasolikh@mail.com>
2024-09-09 12:28:21 +0500

    js masalalar

---
+++ main.js
@@ -0,0 +1,46 @@
+//  1-masala
+
+
+// let n = 10;
+
+// for (let i = 1 ; i <= n ; i++){
+//     console.log(i)
+// }
+
+
+
+
+// 2-masala
+
+
+// let n = 0
+ 
+// for(let i = 10; i > n ; i--){
+//     console.log(i)
+// }
+
+
+
+
+// 3-masala
+
+
+// let n = 20
+
+// for(let i = 1 ; i < n ; i++){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+// }
+
+
+
+
+// 4-masala
+
+// for(let i = 1 ; i < n ; i++){
+//     if(i%2==1){
+//         console.log(i)
+//     }
+// }
+
